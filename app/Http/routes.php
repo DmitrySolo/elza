089c82316f0396729cefb630aabdfbe7e7d0f84c
DmitrySolo <solo-webworks@mail.ru>
2016-07-25 17:32:28 +0300

    commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -62,10 +62,17 @@ Route::group(['middleware' => ['web']], function () {
     Route::post('/ajax/addcdekelem','AjaxFormController@newCDEKelem');
     Route::get('/settask','WelcomeController@setTask');
     Route::get('/rv','TaskController@reviewTasks');
+    Route::post('/ajax/searchSteps','SearchStatsController@getSearchSteps');
+    Route::post('/ajax/runSearchStep','SearchStatsController@runSearchStep');
+    Route::get('/search','PageBuilderController@getSearchResult');
+    Route::get('/ajax/searchStats','SearchStatsController@searchStats');
+    Route::get('/searchman','PageBuilderController@searchManager');
+    Route::post('/searchman','PageBuilderController@searchManager');
+    Route::get('/checkcaptcha','SearchStatsController@yaRestore');
+    Route::post('/checkcaptcha','SearchStatsController@yaRestore');
     Route::get('/rds','PageBuilderController@getRDSList');
     Route::get('/bitrix','PageBuilderController@getBitrixList');
     Route::post('/ajax/changeTaskStatus','AjaxFormController@changeTaskStatus');
     Route::post('/ajax/DoneTask','AjaxFormController@completeTask');
-    Route::get('/search_rating','PageBuilderController@searchRating');
+    Route::get('/print/{number}/print.pdf','CDEKController@orderPrint');
 });
-
