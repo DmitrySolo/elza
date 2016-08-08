e33d1b398b2e0ae73df37d952bdf0cadf17f550c
SERGK393 <sergk393@inbox.ru>
2016-08-08 13:44:32 +0300

    product stats
    - stats by city

--- resources/views/child/productStats.php
+++ resources/views/child/productStats.php
@@ -3,6 +3,14 @@
         <div class="col-xs-12">
             <label>Дата начала:<input class="dateRDS form-control" type="text" name="dateFirst" autocomplete="off" value="<?=$form['dateFirst']?>"></label>
             <label>Дата окончания:<input class="dateRDS form-control" type="text" name="dateLast" autocomplete="off" value="<?=$form['dateLast']?>"></label>
+            <label>Город:<select class="form-control" name="city">
+                    <option value="!empty!">Выберите город</option>
+                    <?php foreach($cities as $city){?>
+                        <option <?php if(isset($form['city'])){
+                            if($city->city==$form['city']) echo 'selected';
+                        }?>><?php echo $city->city;?></option>
+                    <?php }?>
+                </select></label>
         </div>
     </div>
     <div class="form-group row">
@@ -64,7 +72,7 @@
                     <ul class="stats">
                         <?php foreach($categories as $num=>$category):?>
                             <li <?=empty($num)?' style="text-decoration: underline"':''?>>
-                                <?=round($category->profit/$category->sum_price*100,2)?>%
+                                <?=empty($category->sum_price)?0:round($category->profit/$category->sum_price*100,2)?>%
                             </li>
                         <?php endforeach; ?>
                     </ul>
@@ -73,7 +81,7 @@
                     <ul class="stats">
                         <?php foreach($categories as $num=>$category):?>
                             <li <?=empty($num)?' style="text-decoration: underline"':''?>>
-                                <b><?=number_format($category->profit/$category->sum_quantity, 2, ',', ' ')?> руб.</b>
+                                <b><?=number_format(empty($category->sum_quantity)?0:($category->profit/$category->sum_quantity), 2, ',', ' ')?> руб.</b>
                             </li>
                         <?php endforeach; ?>
                     </ul>
@@ -85,8 +93,8 @@
         <td><b><?=number_format($data['category_all']['sum_price'], 2, ',', ' ')?> руб.</b></td>
         <td><?=$data['category_all']['sum_quantity']?></td>
         <td><b><?=number_format($data['category_all']['profit'], 2, ',', ' ')?> руб.</b></td>
-        <td><?=round($data['category_all']['profit']/$data['category_all']['sum_price']*100,2)?>%</td>
-        <td><b><?=number_format($data['category_all']['profit']/$data['category_all']['sum_quantity'], 2, ',', ' ')?> руб.</b></td>
+        <td><?=empty($data['category_all']['sum_price'])?0:round($data['category_all']['profit']/$data['category_all']['sum_price']*100,2)?>%</td>
+        <td><b><?=number_format(empty($data['category_all']['sum_quantity'])?0:($data['category_all']['profit']/$data['category_all']['sum_quantity']), 2, ',', ' ')?> руб.</b></td>
     </tr>
 </table>
 <?php else: ?>
@@ -108,8 +116,8 @@
             <td><b><?=number_format($brand->sum_price, 2, ',', ' ')?> руб.</b></td>
             <td><?=$brand->sum_quantity?></td>
             <td><b><?=number_format($brand->profit, 2, ',', ' ')?> руб.</b></td>
-            <td><?=round($brand->profit/$brand->sum_price*100,2)?>%</td>
-            <td><b><?=number_format($brand->profit/$brand->sum_quantity, 2, ',', ' ')?> руб.</b></td>
+            <td><?=empty($brand->sum_price)?0:round($brand->profit/$brand->sum_price*100,2)?>%</td>
+            <td><b><?=number_format(empty($brand->sum_quantity)?0:($brand->profit/$brand->sum_quantity), 2, ',', ' ')?> руб.</b></td>
         </tr>
     <?php endforeach; ?>
     <tr class="danger">
@@ -117,10 +125,42 @@
         <td><b><?=number_format($data['brand_all']['sum_price'], 2, ',', ' ')?> руб.</b></td>
         <td><?=$data['brand_all']['sum_quantity']?></td>
         <td><b><?=number_format($data['brand_all']['profit'], 2, ',', ' ')?> руб.</b></td>
-        <td><?=round($data['brand_all']['profit']/$data['brand_all']['sum_price']*100,2)?>%</td>
-        <td><b><?=number_format($data['brand_all']['profit']/$data['brand_all']['sum_quantity'], 2, ',', ' ')?> руб.</b></td>
+        <td><?=empty($data['brand_all']['sum_price'])?0:round($data['brand_all']['profit']/$data['brand_all']['sum_price']*100,2)?>%</td>
+        <td><b><?=number_format(empty($data['brand_all']['sum_quantity'])?0:($data['brand_all']['profit']/$data['brand_all']['sum_quantity']), 2, ',', ' ')?> руб.</b></td>
     </tr>
 </table>
+<?php else: ?>
+    <h3>нет данных</h3>
+<?php endif; ?>
+<h2>Анализ результатов по городам</h2>
+<?if(isset($data['city'])):?>
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>№</th><th>Город</th><th>Сумма</th><th>Количество</th><th>Профит</th><th>% профита</th><th>Ср. профит</th>
+        </tr>
+        </thead>
+        <?$n=0?>
+        <?php foreach($data['city'] as $city):?>
+            <tr class="warning">
+                <td><?=++$n?>.</td>
+                <td><a href="/stats?city=<?=$city->city?>&dateFirst=<?=$form['dateFirst']?>&dateLast=<?=$form['dateLast']?>"><?=empty($city->city)?'Без города':$city->city?></a></td>
+                <td><b><?=number_format($city->sum_price, 2, ',', ' ')?> руб.</b></td>
+                <td><?=$city->sum_quantity?></td>
+                <td><b><?=number_format($city->profit, 2, ',', ' ')?> руб.</b></td>
+                <td><?=empty($city->sum_price)?0:round($city->profit/$city->sum_price*100,2)?>%</td>
+                <td><b><?=number_format(empty($city->sum_quantity)?0:($city->profit/$city->sum_quantity), 2, ',', ' ')?> руб.</b></td>
+            </tr>
+        <?php endforeach; ?>
+        <tr class="danger">
+            <td></td><td align="right">ИТОГО:</td>
+            <td><b><?=number_format($data['city_all']['sum_price'], 2, ',', ' ')?> руб.</b></td>
+            <td><?=$data['city_all']['sum_quantity']?></td>
+            <td><b><?=number_format($data['city_all']['profit'], 2, ',', ' ')?> руб.</b></td>
+            <td><?=empty($data['city_all']['sum_price'])?0:round($data['city_all']['profit']/$data['city_all']['sum_price']*100,2)?>%</td>
+            <td><b><?=number_format(empty($data['city_all']['sum_quantity'])?0:($data['city_all']['profit']/$data['city_all']['sum_quantity']), 2, ',', ' ')?> руб.</b></td>
+        </tr>
+    </table>
 <?php else: ?>
     <h3>нет данных</h3>
 <?php endif; ?>
