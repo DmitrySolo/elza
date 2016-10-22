2ef34a44d4c891c8f0fee9c50313d3d57c765eb7
DmitrySolo <solo-webworks@mail.ru>
2016-10-22 11:53:14 +0300

    update of 22.10.2016 11:45

--- resources/views/child/footer.php
+++ resources/views/child/footer.php
@@ -1,6 +1,6 @@
 <?if(isset($rds)){?>
     <div style="padding:6px 0 6px 10%">
-        <h5 style="display: inline-block">Итого по выбранному периоду: Профит:<span style="color:green"> <?=$rds['endTotal']?>р.</span>, в ожидании: <span style="color: #31b0d5;"><?=$rds['processTotal']?>р.</span></h5>
+<h5 style="display: inline-block">Итого по выбранному периоду: Профит:<span style="color:green"> <?=$rds['endTotal']?>р.</span>, в ожидании: <span style="color: #31b0d5;"><?=$rds['processTotal']?>р.</span></h5>
 Вручено: <span style="color:green"><?=$rds['st4']?>%</span> Отказов: <span style="color: red"><?=$rds['st5']?>%</span><button class="btn-default btn btn-xs btn-warning" data-toggle="modal" data-target="#reason">(Причины)</button></div>
                     <div id="reasonCtn">
                         <?php foreach($rds['reason'] as $reason=>$count) echo "$reason : $count <br>";?>
